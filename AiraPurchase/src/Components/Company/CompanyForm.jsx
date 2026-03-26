43b4aaa1ad5820e981d4daa34f8392727614b3ab
SHYAMJSDABHI <shyamjsdabhi2367@gmail.com>
2026-03-26 15:33:43 +0530

    change the btn color

--- AiraPurchase/src/Components/Company/CompanyForm.jsx
+++ AiraPurchase/src/Components/Company/CompanyForm.jsx
@@ -108,7 +108,7 @@ function CompanyForm() {
 
         <div className="d-flex justify-content-between">
           <button
-            className="btn btn-secondary"
+            className="btn btn-primary"
             type="button"
             onClick={() => navigate('/dashboard/companymaster')}
           >
